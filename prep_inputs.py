41550681e79562ec7bf122b1f79bd66f73492423
victorymr <38334523+victorymr@users.noreply.github.com>
2021-03-23 18:32:35 -0400

    Update prep_inputs.py

--- prep_inputs.py
+++ prep_inputs.py
@@ -227,10 +227,10 @@ def value_inputs():
     ## Relevant Industry Metrics
     #display(widgets.HTML('<h4> Key Industry Metrics - Use as Reference </h4>'))
     print(ind_df)
-    out_gen.append_stdout(widgets.HTML('<h4> Metrics from Company Recent Financials </h4>'))
+    out_gen.append(widgets.HTML('<h4> Metrics from Company Recent Financials </h4>'))
     out_gen.append_display_data(pd.DataFrame(data=list_dict.values(),
                            index=list_dict.keys(),columns=[comp.ticksym]))
-    out_gen.append_stdout(widgets.HTML('<h4> Key Industry Metrics - Use as Reference </h4>'))
+    out_gen.append(widgets.HTML('<h4> Key Industry Metrics - Use as Reference </h4>'))
     out_gen.append_display_data(ind_df)
     
     ## Relevant Country of operation Metrics
