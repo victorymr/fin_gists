63492296263283bfa44f13ac390e008007b64cbd
victorymr <38334523+victorymr@users.noreply.github.com>
2021-03-24 13:09:14 -0400

    Update prep_inputs.py

--- prep_inputs.py
+++ prep_inputs.py
@@ -144,9 +144,9 @@ def get_lease_opt():
 
   ## Options inputs
   opdict = opt_dict = {} # we may have some duplication here.
-  opdict['strike'] = widgets.FloatText(description = 'Avg Strike', value = dfos['Strike'],style=style)
-  opdict['expiration'] = widgets.FloatText(description = 'Avg Expiration', value = dfos['AvgMaturity'],style=style)
-  opdict['n_options'] = widgets.FloatText(description = 'Num of Options', value = dfos['NumOptions'],style=style)
+  opdict['strike'] = widgets.FloatText(description = 'Avg Strike', value = dfos['strike'],style=style)
+  opdict['expiration'] = widgets.FloatText(description = 'Avg Expiration', value = dfos['expiration'],style=style)
+  opdict['n_options'] = widgets.FloatText(description = 'Num of Options', value = dfos['n_options'],style=style)
   def foptions(**opdict):
     opt_dict['strike'] = opdict['strike']
     opt_dict['expiration'] = opdict['expiration']
