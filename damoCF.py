5a5881075d7642b9cea943dd8d98ec9129719ce8
victorymr <38334523+victorymr@users.noreply.github.com>
2021-03-24 21:34:38 -0400

    Update damoCF.py

--- damoCF.py
+++ damoCF.py
@@ -292,9 +292,9 @@ def plot_sim():
   sns.set_style('darkgrid')
   sns.histplot(sim_df['equity_val_pershare'],stat='probability')
   ax.text(sv.comp.info['previousClose'],0.2,'* prevClose',rotation=60)
-  ax.text(cfdict['equity_val_pershare'],0.2,'* Intrinsic Value',rotation=60)
+  #ax.text(cfdict['equity_val_pershare'],0.2,'* Intrinsic Value',rotation=60)
   ax.plot([sv.comp.info['fiftyTwoWeekHigh'],sv.comp.info['fiftyTwoWeekLow']],[.10,.10],color='r')
-  plt.show()
+  plt.show():
 
 def run_sim(comp,Inp_dict,nsim=100):
   simlist = []
