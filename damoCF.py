20e854bc3a416b8fdc3889fbd1d0295acfd2573f
victorymr <38334523+victorymr@users.noreply.github.com>
2021-03-24 21:57:44 -0400

    Update damoCF.py

--- damoCF.py
+++ damoCF.py
@@ -290,7 +290,7 @@ def plot_sim(sim_df,cfdict):
   sns.set_style('darkgrid')
   sns.histplot(sim_df['equity_val_pershare'],stat='probability')
   ax.text(sv.comp.info['previousClose'],0.2,'* prevClose',rotation=60)
-  #ax.text(cfdict['equity_val_pershare'],0.2,'* Intrinsic Value',rotation=60)
+  ax.text(cfdict['equity_val_pershare'],0.2,'* Intrinsic Value',rotation=60)
   ax.plot([sv.comp.info['fiftyTwoWeekHigh'],sv.comp.info['fiftyTwoWeekLow']],[.10,.10],color='r')
   #plt.show();
   return ax
@@ -308,9 +308,9 @@ def run_sim(comp,Inp_dict,nsim=100):
     simlist.append(rowlist)
   sim_df = pd.DataFrame(simlist,columns=selcols)
   cfdict = calc_cashflow(comp,Inp_dict,sim={'Do':0,'Vol':5})
-  ax = plot_sim(sim_df,cfdict)
-  #fig, ax = plt.subplots(1, 1)
-  #sim_df[['long_term_cagr','long_term_coc','long_term_margin']].plot(title='Simulation variables')
+  plot_sim(sim_df,cfdict)
+  fig, ax = plt.subplots(1, 1)
+  sim_df[['long_term_cagr','long_term_coc','long_term_margin']].plot(title='Simulation variables')
   #plt.show()
   return sim_df
 
