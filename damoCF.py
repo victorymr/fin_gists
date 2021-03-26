27c56a0ee59de155c50b223d5c797669da160480
victorymr <38334523+victorymr@users.noreply.github.com>
2021-03-25 21:47:14 -0400

    Update damoCF.py

--- damoCF.py
+++ damoCF.py
@@ -129,15 +129,16 @@ def lease_conv(lease_inp,cost_of_debt=0.03,nyrs_bulk=3):
 ## R&D converter
 #3 we want to convert it from a opex to capital asset
 def rnd_conv(comp):
-  rnd = comp.financials.loc['Research Development']
+  rnd = comp.rnd
+  tax_rate = comp.taz_rate
   nyrs = len(rnd)
   amort = rnd[1:]*(1/max(nyrs-1,1)) # current year from prev
   unamort = rnd*(np.arange(nyrs,0,-1)-1)/(nyrs-1)
 
-  adj_op_income = rnd[0]-sum(amort)
+  adj_op_income = comp.rnd[0]-sum(amort)
   rnd_asset = sum(unamort)
 
-  tax_rate = np.mean(comp.financials.loc['Income Tax Expense']/comp.financials.loc['Ebit'])
+  #tax_rate = np.mean(comp.financials.loc['Income Tax Expense']/comp.financials.loc['Ebit'])
   tax_eff_rnd = adj_op_income*tax_rate
   rnd_dict = {'rnd_asset': rnd_asset, 'adj_op_income': adj_op_income, 'tax_eff_rnd': tax_eff_rnd}
   return rnd_dict
