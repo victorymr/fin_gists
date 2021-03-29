d136f7bafbac1cb0d31a835abea3ca0a3aea8e5c
victorymr <38334523+victorymr@users.noreply.github.com>
2021-03-29 15:43:54 -0400

    Update prep_inputs.py

--- prep_inputs.py
+++ prep_inputs.py
@@ -279,7 +279,7 @@ def value_inputs():
     for k,v in story_dict.items():
       sv.Inp_dict[k] = v
   story_out = widgets.interactive_output(fstory, story_dict)
-  story_ui = widget.VBox(tuple(story_dict))
+  story_ui = widgets.VBox(tuple(story_dict))
   display(story_ui,story_out)
 
   #out_gen = widgets.Output(layout={'border': '1px solid black'},wait=True)
