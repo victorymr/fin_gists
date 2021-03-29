fadfbb5b83404ebb2b0dee73beb54087650a41ec
victorymr <38334523+victorymr@users.noreply.github.com>
2021-03-28 22:48:06 -0400

    Update damoCF.py

--- damoCF.py
+++ damoCF.py
@@ -312,6 +312,24 @@ def calc_cashflow(comp,ID,sim={'Do':0, 'Vol':5}):
   return cfdict
 
 def mk_waterfall():
+  import plotly.graph_objects as go
+
+  fig = go.Figure(go.Waterfall(
+      name = "20", orientation = "v",
+      measure = ["relative", "relative", "total", "relative", "relative", "total"],
+      x = ["Sales", "Consulting", "Net revenue", "Purchases", "Other expenses", "Profit before tax"],
+      textposition = "outside",
+      text = ["+60", "+80", "", "-40", "-20", "Total"],
+      y = [60, 80, 0, -40, -20, 0],
+      connector = {"line":{"color":"rgb(63, 63, 63)"}},
+  ))
+
+  fig.update_layout(
+          title = "Equity Value Water Fall",
+          showlegend = True
+  )
+  fig.show()
+  return
   
 def plot_sim(sim_df,cfdict,comp=sv.comp):
   ## Plot the histogram
