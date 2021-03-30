6174773779dbf3f418c74d34e4045b68e8a156fc
victorymr <38334523+victorymr@users.noreply.github.com>
2021-03-30 16:30:41 -0400

    Update damoCF.py

--- damoCF.py
+++ damoCF.py
@@ -288,14 +288,14 @@ def calc_cashflow(comp,ID,sim={'Do':0, 'Vol':5}):
     #print pretty cashflow - $s in mill others in %
     tmp_cf = cashflow
     listofmill = ['rev_fcst','EBIT','Reinvestment','NOL','EBITafterTax','FCFF','PVFCFF','InvestedCapital']
-    currdict = dict(zip(listofmill,["${:,.2f}"]*len(listofmill)))
+    form_dict = dict(zip(listofmill,["${:,.2f}"]*len(listofmill)))
     perclist = list(set(cashflow.columns.tolist())-set(listofmill))
     percdict = dict(zip(perclist,["{:.2f}%"]*len(perclist)))
     tmp_cf[listofmill] = cashflow[listofmill]/1e6
     #print(tmp_cf)
     #format_mapping = {"Currency": "${:,.2f}", "Int": "{:,.0f}", "Rate": "{:.2f}%"}
-    format_mapping = currdict.update(percdict)
-    display(tmp_cf.transpose().style.format(format_mapping))
+    form_dict.update(percdict)
+    display(tmp_cf.transpose().style.format(form_dict))
     #print waterfall
     wf_dict = {'pv_terminal_value': [pv_terminal_value, 'relative'],
                'pv_CFNyr': [pv_CFNyr, 'relative'],
