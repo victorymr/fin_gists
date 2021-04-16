2f2eed51a4dbaac79c8abc8d3c8c0d76f3c6a56f
victorymr <38334523+victorymr@users.noreply.github.com>
2021-04-16 18:29:17 -0400

    Update damoCF.py

--- damoCF.py
+++ damoCF.py
@@ -442,7 +442,7 @@ def run_sim(comp,Inp_dict,nsim=100):
     simlist.append(rowlist)
   sim_df = pd.DataFrame(simlist,columns=selcols)
   cfdict = calc_cashflow(comp,Inp_dict,sim={'Do':0,'Vol':5})
-  plot_sim(sim_df,cfdict)
+  plot_sim(sim_df,cfdict,sv.comp)
   sim_df[['long_term_cagr','long_term_coc','long_term_margin']].plot(title='Simulation variables')
   plt.show()
   return sim_df
